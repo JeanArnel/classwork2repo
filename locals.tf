cbbdc6dbf8f2d054f6ddad64d8e30f4cdce8c533
JeanArnel <168039393+JeanArnel@users.noreply.github.com>
2024-05-22 20:56:22 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
-//virtual_machines_config = yamldecode(file("${path.module}/configs/virtual_machines.yaml"))
+locals{
+virtual_machines_config = yamldecode(file("${path.module}/configs/virtual_machines.yaml"))
 virtual_machines_map = { for vm in local.virtual_machines_config.virtual_machines : vm.name => vm }
 address_space = ["10.0.0.0/16"]
 subnet_prefix = ["10.0.2.0/24"]
@@ -7,4 +8,5 @@ vm_size = "Standard_DS1_v2"
   os_offer     = "0001-com-ubuntu-server-jammy"
   os_sku       = "22_04-lts"
   os_version   = "latest"
+}
 }
